979f6a4148d4883ebef0eafa3f89750f689687f3
nurshafasalsabila <117736386+nurshafasalsabila@users.noreply.github.com>
2022-11-15 01:31:21 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Paswa
+# Paswa
+# Paswa
---
+++ index.php
@@ -0,0 +1,33 @@
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>PASWA 2022</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="assets/css/sb-admin-2.css" rel="stylesheet">
+</head>
+
+<body class="bg-gradient-info">
+    <div class="container"> 
+        <h1 class="text-center text-white">Selamat Datang di Website Paswa 2022</h1>
+        <div class="row justify-content-around">
+            <a class="btn btn-primary" href="login.php">Login</a>
+            <a class="btn btn-primary" href="register.php">Register</a>
+        </div>
+        <div class="col-lg-6 d-none d-lg-block">
+            <img src="gambar/bt21.jpeg" style="width: 1100px;height: 550px;">
+        </div>
+    </div>
+</body>
+
+</html>
